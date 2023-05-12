88d3a3361398533cec9d89788f6fdb4ebec6dc17
JulioCesarTeixeira <juliocesarteixeira1992@gmail.com>
2023-05-12 11:54:33 +0100

    Added path mapping and logos

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,28 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo"],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          root: ["./src"],
+          extensions: [".js", ".jsx", ".ts", ".tsx", ".json"],
+          alias: {
+            "@components": "./src/components",
+            "@screens": "./src/screens",
+            "@assets": "./src/assets",
+            "@storage": "./src/storage",
+            "@utils": "./src/utils",
+            "@theme": "./src/theme",
+            "@hooks": "./src/hooks",
+            "@routes": "./src/routes",
+            "@constants": "./src/constants",
+            "@services": "./src/services",
+            "@types": "./src/types",
+          },
+        },
+      ],
+    ],
   };
 };
